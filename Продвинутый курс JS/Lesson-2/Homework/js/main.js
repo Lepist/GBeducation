ec33f45a2dc9440d46e9a2dc1470bdd2196b6749
Дамир <rootfix25@gmail.com>
2021-06-23 13:50:26 +0300

    HW 2 DONE

--- Продвинутый курс JS/Lesson-2/Homework/js/main.js
+++ Продвинутый курс JS/Lesson-2/Homework/js/main.js
@@ -2,26 +2,28 @@ const products = [
     {id: 1, title: 'Notebook', price: 2000},
     {id: 2, title: 'Mouse', price: 20},
     {id: 3, title: 'Keyboard', price: 200},
-    {id: 4, title: 'Gamepad', price: 50},
+    {id: 4, title: 'Gamepad', price: 50}
 ];
 //Функция для формирования верстки каждого товара
-const renderProduct = product => {
+const renderProduct = (product,img='https://via.placeholder.com/200x150') => {
     return `<div class="product-item">
-                <div class="item-photo"></div>
+                <img src="${img}">
                 <h3>${product.title}</h3>
-                <p>Цена: ${product.price} р.</p>
+                <p>${product.price}</p>
                 <button class="buy-btn">Купить</button>
             </div>`
 };
 const renderPage = list => {
-/*    let productsList = list.map(item => renderProduct(item.title, item.price));
-    let joinProductList = productsList.join("");
-    console.log(joinProductList);
-    document.querySelector('.products').innerHTML = joinProductList;
-    // Мой вариант    
-*/
     document.querySelector('.products').innerHTML = list.map(item => renderProduct(item)).join('');
-    // вариант увиденный на 2 уроке(разобрался)
 };
 
 renderPage(products);
+
+const getFullPriceOfGoods = () => {
+    let sum = 0;
+    products.forEach((product) =>{
+        sum += product.price;
+    });
+    alert(sum);
+}
+getFullPriceOfGoods(products);
